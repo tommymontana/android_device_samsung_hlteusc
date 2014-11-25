02f96be9778f760f9ad43127cb1fb81da46c191d
Tommy <tommymontana210@gmail.com>
2014-11-25 16:58:16 -0500

    remove conflicting cdma_properties

--- init/init_hlteusc.c
+++ init/init_hlteusc.c
@@ -59,7 +59,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
       	property_set("ro.build.fingerprint", "samsung/hlteusc/hlteusc:4.4.2/KOT49H/N900R4TYUCNAB:user/release-keys");
 	property_set("ro.build.description", "hlteusc-user 4.4.2 	 KOT49H N900R4TYUCNAB release-keys");
 	property_set("ro.product.model", "SM-N900R4");
-	cdma_properties("0", "311580", "U.S.Cellular");
        
     }
     property_get("ro.product.device", device);
