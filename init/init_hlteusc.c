790187b91389dc3021360b09d16ae8874ecc3b94
Tommy <tommymontana210@gmail.com>
2014-12-11 11:16:15 -0500

    fix data drop when receiving mms

--- init/init_hlteusc.c
+++ init/init_hlteusc.c
@@ -60,6 +60,7 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 	property_set("ro.build.description", "hlteusc-user 4.4.2 	 KOT49H N900R4TYUCNAB release-keys");
 	property_set("ro.product.model", "SM-N900R4");
         property_set("ro.product.device", "hlteusc");
+	property_set("telephony.sms.pseudo_multipart", "1");
         cdma_properties("1", "311580", "US Cellular");
     }
     property_get("ro.product.device", device);
